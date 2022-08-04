864f8a09e0a721c818e78f716347b2661fe5035d
Kisaru1523 <topruning589@gmail.com>
2022-08-04 14:07:50 +0700

    finish

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 async function callApi() {
+  const name = document.getElementById("name");
   const resp = await axios.get("https://randomuser.me/api/");
+  name.innerHTML = response.data.name;
 }
+
+const getBtn = document.getElementById("btn-random");
+const getname = document.getElementById("name");
+const getrecover = document.getElementById("recover");
+const getdeaths = document.getElementById("deaths");
+const getupdatedate = document.getElementById("updatedate");
+const getdata = () => {
+  axios.get("https://randomuser.me/api/").then((response) => {
+    console.log(response);
+    getname.innerHTML = response.data.name;
+    getrecover.innerHTML = response.data.NewRecovered;
+    getdeaths.innerHTML = response.data.NewDeaths;
+    getupdatedate.innerHTML = response.data.UpdateDate;
+  });
+};
+getBtn.addEventListener("click", getdata);
